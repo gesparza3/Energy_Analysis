60df553062df93200d6157713192d04121a08a9f
gesparza3 <gesparza3@mail.csuchico.edu>
2018-04-13 11:50:16 -0700

    Working on fixing random effects model

--- scripts/model_building.R
+++ scripts/model_building.R
@@ -6,6 +6,7 @@
 library(lme4)
 library(sjPlot)
 library(ggplot2)
+library(dplyr)
 
 ################################################################################
 
@@ -19,11 +20,11 @@ states.energy$GDP.growth <- as.factor(states.energy$GDP.growth)
 ## GLM
 
 ## Partial Pooling
-glm.model <- glmer(GDP.growth ~ hydro.growth + (hydro.growth | State),
+glm.model <- glmer(GDP.growth ~ -1 + motor_gas.growth + (1 | State),
                family=binomial("logit"), data=states.energy)
 
 ## Make sjPlot for random effects
-sjp.glmer(glm.model, type="rs.ri", y.offset = .4)
+sjp.glmer(glm.model, type="re", y.offset = .4)
 
 ## Make sjPlot for fixed effects
 sjp.glmer(glm.model, type="fe", y.offset = .4)
@@ -55,9 +56,23 @@ ggplot(states.energy, aes(x=GDP.growth, fill=State)) + geom_bar() +
 ggplot(states.energy, aes(State, fill=GDP.growth)) + geom_bar() + coord_flip()
 
 ## Plot hydro.growth
-newer <- filter(states.energy, states.energy$State == "AK")
-ggplot(states.energy, aes(log(hydro.growth))) + geom_histogram()
+newer <- filter(states.energy, states.energy$State == "MI")
+ggplot(newer, aes(hydro.growth)) + geom_histogram(bins=10)
 
 ## Plot Coal.growth
-ggplot(states.energy, aes(x=hydro.growth, fill=State)) + 
+ggplot(states.energy, aes(x=hydro.growth, fill=State)) +
   geom_density() + facet_wrap(~State) + theme(legend.position = 'none')
+
+################################################################################
+
+## Fixed Effect model
+model.glm <- glm(GDP.growth ~ coal.growth + State, family="binomial",
+                 states.energy)
+anova(model.glm)
+################################################################################
+
+theta <- getME(glm.model,"theta")
+## diagonal elements are identifiable because they are fitted
+##  with a lower bound of zero ...
+diag.element <- getME(glm.model,"lower")==0
+any(theta[diag.element]<1e-5)
